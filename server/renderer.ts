8a8b5f46669a27c44547d186a500b6e797331345
Jake Saparito <saparitoj@gmail.com>
2026-03-22 12:08:30 -0400

    Again

--- server/renderer.ts
+++ server/renderer.ts
@@ -3,46 +3,45 @@
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ThemeModule = {
-  render: (resume: any, options?: any) => string | Promise<string>
-}
+  render: (resume: any, options?: any) => string | Promise<string>;
+};
 
 const INSTALLED_THEMES: Record<string, () => Promise<ThemeModule>> = {
   // Original themes
-  stackoverflow: () => import('jsonresume-theme-stackoverflow'),
-  even: () => import('jsonresume-theme-even'),
-  paper: () => import('jsonresume-theme-paper'),
-  spartan: () => import('jsonresume-theme-spartan'),
-  flat: () => import('jsonresume-theme-flat'),
-  orbit: () => import('jsonresume-theme-orbit'),
-  onepage: () => import('jsonresume-theme-onepage'),
-  cora: () => import('jsonresume-theme-cora'),
+  stackoverflow: () => import("jsonresume-theme-stackoverflow"),
+  even: () => import("jsonresume-theme-even"),
+  paper: () => import("jsonresume-theme-paper"),
+  spartan: () => import("jsonresume-theme-spartan"),
+  flat: () => import("jsonresume-theme-flat"),
+  orbit: () => import("jsonresume-theme-orbit"),
+  onepage: () => import("jsonresume-theme-onepage"),
+  cora: () => import("jsonresume-theme-cora"),
   // Added themes
-  modern: () => import('jsonresume-theme-modern'),
-  classy: () => import('jsonresume-theme-classy'),
-  straightforward: () => import('jsonresume-theme-straightforward'),
-  catppuccin: () => import('jsonresume-theme-catppuccin'),
-  sceptile: () => import('jsonresume-theme-sceptile'),
-  'modern-extended': () => import('jsonresume-theme-modern-extended'),
-  simplyelegant: () => import('jsonresume-theme-simplyelegant'),
-  'timeline-fixed': () => import('jsonresume-theme-timeline-fixed'),
-  msresume: () => import('jsonresume-theme-msresume'),
-  rnord: () => import('jsonresume-theme-rnord'),
-  projects: () => import('jsonresume-theme-projects'),
-  printclassy: () => import('jsonresume-theme-printclassy'),
-}
+  modern: () => import("jsonresume-theme-modern"),
+  classy: () => import("jsonresume-theme-classy"),
+  straightforward: () => import("jsonresume-theme-straightforward"),
+  catppuccin: () => import("jsonresume-theme-catppuccin"),
+  sceptile: () => import("jsonresume-theme-sceptile"),
+  "modern-extended": () => import("jsonresume-theme-modern-extended"),
+  simplyelegant: () => import("jsonresume-theme-simplyelegant"),
+  msresume: () => import("jsonresume-theme-msresume"),
+  rnord: () => import("jsonresume-theme-rnord"),
+  projects: () => import("jsonresume-theme-projects"),
+  printclassy: () => import("jsonresume-theme-printclassy"),
+};
 
-const FALLBACK_THEME = 'stackoverflow'
+const FALLBACK_THEME = "stackoverflow";
 
 export async function renderResume(resume: unknown, themeSlug: string): Promise<string> {
-  const loader = INSTALLED_THEMES[themeSlug] ?? INSTALLED_THEMES[FALLBACK_THEME]
+  const loader = INSTALLED_THEMES[themeSlug] ?? INSTALLED_THEMES[FALLBACK_THEME];
 
-  let themeModule: ThemeModule
+  let themeModule: ThemeModule;
   try {
-    themeModule = await loader()
+    themeModule = await loader();
   } catch {
-    themeModule = await INSTALLED_THEMES[FALLBACK_THEME]()
+    themeModule = await INSTALLED_THEMES[FALLBACK_THEME]();
   }
 
-  const result = await themeModule.render(resume, {})
-  return result
+  const result = await themeModule.render(resume, {});
+  return result;
 }
